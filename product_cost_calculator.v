344c7c850db33687e2f841a6937e9fef61880fde
Ritvik Aggarwal <108319429+Ritvik2103@users.noreply.github.com>
2024-12-07 15:12:43 +0530

    Update product_cost_calculator.v

--- product_cost_calculator.v
+++ product_cost_calculator.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.11.2024 10:39:26
-// Design Name: 
-// Module Name: product_cost_calculator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 module product_cost_calculator(product_sel, product_cost);
     input  [1:0] product_sel;      // Product selection
